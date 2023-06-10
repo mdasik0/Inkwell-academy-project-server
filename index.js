84865252c22e1f418379c7394be494eca7754035
mdasik0 <asikthe1st@gmail.com>
2023-06-10 20:49:57 +0600

    promote to admin added

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const app = express()
-const port = process.env.PORT || 5000 ;
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.send('summer Camp Server is running')
-})
+app.get("/", (req, res) => {
+  res.send("summer Camp Server is running");
+});
 
 app.listen(port, () => {
-    console.log(`This server is running on Port:${port}`)
-})
+  console.log(`This server is running on Port:${port}`);
+});
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.llfgq6f.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,7 +25,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -36,11 +35,43 @@ async function run() {
     // Send a ping to confirm a successful connection
 
     // DataCollections
-    const instructorCollection = client.db("artVenture").collection("instrutors")
+    const allUserCollection = client.db("inkwell").collection("allusers");
 
+    // send a new user Data
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await allUserCollection.findOne(query);
+      if (existingUser) {
+        return res.send((message = "user already exists"));
+      }
+      const result = await allUserCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await allUserCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await allUserCollection.updateOne(filter, updatedoc);
+      res.send(result);
+    });
+    
+    
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
   }
